fb0a7dc4ad043f71478945f2f94c58f1f774d4e5
David Le Hardy <lehardy.david@live.fr>
2017-04-11 17:35:44 +0200

    Modif appPizza

--- JeuAppAngular/app/index.js
+++ JeuAppAngular/app/index.js
@@ -7,9 +7,31 @@ import { RecipesModule } from './users';
 angular.module('app', [
     RecipesModule,
 ])
-    .config(function () {
-        
+    .config(function ($locationProvider) {
 
+        $locationProvider.html5Mode(true);
+
+        $routeProvider
+
+            .when('/', {
+                template: 'views/home.html',
+                controller: 'Home',
+                controllerAs: 'ctrl',
+            })
+
+            .when('/game', {
+                template: 'views/game.html',
+                controller: 'RecipesControler',
+                controllerAs: 'ctrl',
+            })
+            
+            .otherwise({
+                redirectTo: "/"
+            })
+
+    })
+
+    .controller('Home', function () {
     })
 
     ;
