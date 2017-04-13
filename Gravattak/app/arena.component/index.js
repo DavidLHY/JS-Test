f5ea490f57cc5d3a3fd36eff568a4fc234b64d75
David <lehardy.email@live.fr>
2017-04-13 21:04:35 +0200

    degat zone voisin

--- Gravattak/app/arena.component/index.js
+++ Gravattak/app/arena.component/index.js
@@ -6,45 +6,74 @@ class controller {
         this.team1 = {
             name: "Gentil",
             mode: 'attack',
-            fighters: ['tmoyse@gmail.com', 'paolalambroni@gmail.com','dark.pl@hotmail.fr','alouest_44@yahoo.fr']
+            fighters: ['tmoyse@gmail.com', 'paolalambroni@gmail.com', 'dark.pl@hotmail.fr', 'alouest_44@yahoo.fr']
         };
         this.team2 = {
             name: "Mechant",
             mode: 'defense',
-            fighters: ['gigarelt@gmail.com', 'pouletguerrier@gmail.com','lehardy.david@live.fr','lionel.collidor2016@campus-eni.fr']
+            fighters: ['gigarelt@gmail.com', 'pouletguerrier@gmail.com', 'lehardy.david@live.fr', 'lionel.collidor2016@campus-eni.fr']
         };
         this.attack = false;
+        this.targeted = false;
 
     }
 
     setDamages(damages) {
-        
 
+        console.log("setDamages")
         this.damages = damages.damages;
         this.zone = damages.zone;
         this.attack = true;
 
-
     }
 
-    attackEnd() {
+    attackEnd(email) {
 
         this.attack = false;
+        this.targeted = false;
         this.damages = 0;
-        // @TODO refacto
+
+        if(email.mail === this.email || email.mana)
+        {
         this.team1.mode = this.team1.mode === 'defense' ? 'attack' : 'defense';
         this.team2.mode = this.team2.mode === 'defense' ? 'attack' : 'defense';
+        }
     }
 
-    getTarget(email)
-    {
-        this.email =email;
+    getTarget(email) {
+        this.email = email;
+        this.targeted = true;
+        this.index = (this.team2.fighters.indexOf(email) !== -1)
+            ? this.team2.fighters.indexOf(email)
+            : this.team1.fighters.indexOf(email);
 
-        this.index = fighters.indexOf(email);
-        console.log(this.index);
     }
 
-    
+    getDamages(index) {
+
+        //  console.log("getDamages",this.targeted,index)
+        if (this.targeted) {
+            if (this.zone) {
+                if(Math.abs(index-this.index)<2 )
+                {
+                    return (2-Math.abs(index-this.index))*this.damages;
+                }else{
+                    return 0;
+                }
+                
+
+            } else {
+                if (index === this.index) {
+                    console.log("getDamages", this.targeted, index, this.damages)
+                    return this.damages;
+                } else {
+                    return 0;
+                }
+            }
+        } else {
+            return 0;
+        }
+    }
 
 }
 
--- Gravattak/app/fighter.component/index.js
+++ Gravattak/app/fighter.component/index.js
@@ -9,30 +9,52 @@ class controller {
         this.md5 = md5(this.email);
         this.life = 20 + Math.floor(Math.random() * 30);
         this.mana = 20;
+
+    }
+
+    $onChanges(changes) {
+
+
+        if (changes.damages && this.mode === "defense") {
+
+            if (changes.damages.currentValue) {
+                console.log('change', this.email, changes.damages)
+                this.life = Math.max(this.life - this.damages, 0);
+                this.damages = 0;
+                this.afterInjured({
+                    $event: {mail:this.email}
+                });
+
+            }
+        }
     }
 
     fight() {
+
         this.onFight({
-            $event: { damages:(1 + Math.floor(Math.random() * 5))}
+            $event: { damages: (1 + Math.floor(Math.random() * 5)) }
         });
+
     }
 
     bigfight() {
         this.mana -= 30
         this.onFight({
-            $event: {damages: 10 * (1 + Math.floor(Math.random() * 5))}
+            $event: { damages: 5 * (1 + Math.floor(Math.random() * 5)) }
         });
     }
 
     getMana() {
         this.mana += 20;
-        this.afterInjured();
+        this.afterInjured({
+            $event: {mana: true,mail:this.email}
+        });
     }
 
-    zonalAttack(){
-
+    zonalAttack() {
+        this.mana -= 40;
         this.onFight({
-            $event: {zone:true, damages: 3 * (1 + Math.floor(Math.random() * 5))}
+            $event: { zone: true, damages: (1 + Math.floor(Math.random() * 5)) }
         });
 
     }
@@ -42,12 +64,12 @@ class controller {
         this.getTarget({
             $event: this.email
         })
-
+        //  console.log(this.damages);
         this.life = Math.max(this.life - this.damages, 0);
-        this.afterInjured();
+
     }
 
-     
+
 
 }
 
@@ -59,7 +81,7 @@ export const FighterComponent = {
         mode: '<',
         damages: '<',
         attack: '<',
-        getTarget:"&",
+        getTarget: "&",
     },
     template,
     controller,
